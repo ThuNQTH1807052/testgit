4d513896d30c481c5e8bfe8be4a022d4e024f0c2
ThuNQTH1807052 <thunqth1807052@fpt.edu.vn>
2018-11-06 01:29:51 +0700

    commit lan1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+//    int *contro;
+//    int a = 10;
+//    contro = &a;
+//    printf("%d\n", a);
+//    printf("%d\n", *contro);
+//    printf("%d\n", &a);
+//    printf("%d\n", &contro);
+
+int mang[4];
+int *contro;
+contro = mang;
+    for (int i = 0; i < 4 ; ++i) {
+        printf("%d\n", mang[i + 1]);
+    }
+    return 0;
+}
